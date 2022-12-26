88479af26e1d4191970c3f1203e0384f83bca3b1
lolwngud <118458655+lolwngud@users.noreply.github.com>
2022-12-26 12:40:46 +0900

    tool

---
+++ 42tool/all_clear.sh
@@ -0,0 +1,6 @@
+rm -rf ~/Library/Application\ Support/Code/User/workspaceStorage 
+rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage 
+rm -rf ~/Library/Application\ Support/discord/Cache 
+rm -rf ~/Library/Application\ Support/Code/CachedData 
+rm -rf ~/Library/Developer/CoreSimulator
+rm -rf ~/Library/Caches
